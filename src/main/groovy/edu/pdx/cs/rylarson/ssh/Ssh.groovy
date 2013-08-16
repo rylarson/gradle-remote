d2893277a142206cc76e46a64c60101c09a6be57
Ryan Larson <ryan.mango.larson@gmail.com>
2013-08-15 19:56:32 -0700

    Adding MIT license

--- src/main/groovy/edu/pdx/cs/rylarson/ssh/Ssh.groovy
+++ src/main/groovy/edu/pdx/cs/rylarson/ssh/Ssh.groovy
@@ -1,15 +1,32 @@
 package edu.pdx.cs.rylarson.ssh
 
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Ryan Larson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
-/**
- * Created with IntelliJ IDEA.
- * User: Ryan
- * Date: 8/6/13
- * Time: 5:58 PM
- * To change this template use File | Settings | File Templates.
- */
 class Ssh extends DefaultTask {
 
     def host, username, command, commandResource, port, trust, knownhosts, failonerror, password,
